38da68cfd8a2f6ae64228b3629db6312e38c2e36
maartenpaauw <maartenpaauw@gmail.com>
2018-07-19 17:32:40 +0200

    chore: delete extracted zip directory before download

--- app/Listeners/ExtractZip.php
+++ app/Listeners/ExtractZip.php
@@ -4,6 +4,7 @@ namespace Updater\Listeners;
 
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Support\Facades\Storage;
 
 use Chumper\Zipper\Facades\Zipper;
 
@@ -22,6 +23,9 @@ class ExtractZip implements ShouldQueue
     {
         // The cask.
         $cask = $event->cask;
+
+        // Delete the directory if exists.
+        Storage::deleteDirectory($cask->path . $cask->slug);
 
         // Zipper object.
         $zipper = Zipper::make(storage_path('app/' . $cask->path . $cask->zip_name))->extractTo(storage_path('app/' . $cask->path . $cask->slug));
